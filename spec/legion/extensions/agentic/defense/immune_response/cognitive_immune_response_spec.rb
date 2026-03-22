efe397a505bd068dc6ae8b2651e9d6f2c3ac325f
Esity <matthewdiverson@gmail.com>
2026-03-21 22:29:32 -0500

    fix spec: update immune_response version assertion to 0.1.1

--- spec/legion/extensions/agentic/defense/immune_response/cognitive_immune_response_spec.rb
+++ spec/legion/extensions/agentic/defense/immune_response/cognitive_immune_response_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe Legion::Extensions::Agentic::Defense::ImmuneResponse do
   it 'has a version number' do
-    expect(Legion::Extensions::Agentic::Defense::ImmuneResponse::VERSION).to eq('0.1.0')
+    expect(Legion::Extensions::Agentic::Defense::ImmuneResponse::VERSION).to eq('0.1.1')
   end
 end
